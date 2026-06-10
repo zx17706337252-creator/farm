9562b86b6aa89a28519b378b548c5e70a42f88cf
FarmLife <farmlife@example.com>
2026-06-10 05:25:17 +0000

    Remove unused plugins from root build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,4 @@
-// Root build file for KMM project
-plugins {
-    id("com.android.application") apply false
-    id("com.android.library") apply false
-    id("org.jetbrains.kotlin.android") apply false
-    id("org.jetbrains.kotlin.multiplatform") apply false
-}
-
+// Root build file for FarmLife project
 tasks.register("clean", Delete::class) {
     delete(rootProject.layout.buildDirectory)
 }
