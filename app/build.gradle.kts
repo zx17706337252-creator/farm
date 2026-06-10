0c30b283136b5562a59fd7e851010214121d973f
FarmLife <farmlife@example.com>
2026-06-10 05:19:48 +0000

    Fix gradle plugin version compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("com.google.devtools.ksp")
+    id("com.android.application") version "8.2.0"
+    id("org.jetbrains.kotlin.android") version "1.9.20"
+    id("com.google.devtools.ksp") version "1.9.20-1.0.14"
 }
 
 android {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,8 @@
 pluginManagement {
     repositories {
+        gradlePluginPortal()
         google()
         mavenCentral()
-        gradlePluginPortal()
     }
 }
 
@@ -16,11 +16,3 @@ dependencyResolutionManagement {
 include(":app")
 
 rootProject.name = "FarmLife"
-
-plugins {
-    id("com.android.application") version "8.5.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
-    id("com.android.library") version "8.5.0" apply false
-    id("org.jetbrains.kotlin.multiplatform") version "2.0.0" apply false
-    id("com.google.devtools.ksp") version "2.0.0-1.0.21" apply false
-}
